a593e31b0a660df99d6efacd90cdccc0524b6e26
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-16 18:50:44 +0800

    add sokcets

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -6,13 +9,19 @@ const port = process.env.PORT || 3000;
 // console.log(__dirname + '/../public'); //return ../node-chat-app/server/../public
 
 // console.log(publicPath); //return ../node-chat-app/public
-
-const express = require('express');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath)); //use the file inside public (static refer to sth wont change when every user using)
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect', ()=>{
+        console.log('User was disconneted');
+    })
+});//register a event listener
 
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server is up on port ${port}`);
 });
