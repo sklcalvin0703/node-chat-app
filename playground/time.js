e0828b715e7d531d9d214a9419f9bad9c0387f8b
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-17 17:53:27 +0800

    format timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+// var date = new Date();
+// var month = ['Jan']
+// console.log(date.getMonth());
+var moment = require('moment');
+
+var date = moment(); //curent time
+console.log(date.format('MMM Do, YYYY '));
+
