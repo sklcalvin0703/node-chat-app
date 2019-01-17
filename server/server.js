60553b5e882633b211cc7f97880c1adbb5ac6930
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-17 18:37:58 +0800

    add join page

--- server/server.js
+++ server/server.js
@@ -31,8 +31,7 @@ io.on('connection', (socket)=>{
         console.log('createMessage', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
         callback(); //event acknowledgement
-
-
+        
         //send to other ppl but except yourself
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
